712728a68f6ce01be9ce123428e978cca57c0104
Víctor Mato <87933131+MatoRV@users.noreply.github.com>
2023-12-19 19:10:28 +0100

    Update PriceList.java

--- app/src/main/java/com/politecnicomalaga/NasdaqOilPrices/Model/PriceList.java
+++ app/src/main/java/com/politecnicomalaga/NasdaqOilPrices/Model/PriceList.java
@@ -19,27 +19,4 @@ public class PriceList {
     public void setPriceList(List<Price> priceList) {
         this.priceList = priceList;
     }
-
-    public List<String> getPriceListDays() {
-        List<String> priceListDays = new ArrayList<>();
-
-        return priceListDays;
-    }
-
-    public double getPrecioMayor() {
-        double precioMayor = 0;
-        for (int i = 0; i < this.priceList.size(); i++) {
-            if (Double.parseDouble(this.priceList.get(i).getPrice()) > precioMayor) {
-                precioMayor = Double.parseDouble(this.priceList.get(i).getPrice());
-            }
-        }
-        return precioMayor;
-    }
-
-    public boolean validarDatos() {
-        if ((this.priceList.size() == 0) || (this.priceList.get(0).getDay().equalsIgnoreCase(String.valueOf(LocalDate.now().minusDays(1))))) {
-            return false;
-        }
-        return true;
-    }
 }
